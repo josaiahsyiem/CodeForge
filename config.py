e9da1e5185b24857f50b21318e7fb178710770bb
Josaiah Syiem <josaiahsyiem@gmail.com>
2026-08-02 19:09:14 +0530

    Add GitHub Actions CI, env-var config, geo sandbox, integration docs

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
 # Connection settings for CodeForge's Redis and Postgres
+# Ports can be overridden via environment variables (used in CI).
 
-REDIS_HOST = "localhost"
-REDIS_PORT = 6381          # codeforge-redis
+import os
 
-POSTGRES_HOST = "localhost"
-POSTGRES_PORT = 5434        # codeforge-postgres
-POSTGRES_DB = "codeforge"
-POSTGRES_USER = "postgres"
-POSTGRES_PASSWORD = "codeforge"
+REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.environ.get("REDIS_PORT", "6381"))       # codeforge-redis (local)
+
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "localhost")
+POSTGRES_PORT = int(os.environ.get("POSTGRES_PORT", "5434"))  # codeforge-postgres (local)
+POSTGRES_DB = os.environ.get("POSTGRES_DB", "codeforge")
+POSTGRES_USER = os.environ.get("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "codeforge")
 
 # The Redis list we use as the job queue
 JOB_QUEUE_KEY = "codeforge:jobs"
